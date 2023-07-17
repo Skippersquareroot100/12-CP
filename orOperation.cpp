183e0c20e06db2e104de4ab76181e60ed4f8bda1
Maruf Hasan Anik <111722365+Skippersquareroot100@users.noreply.github.com>
2023-07-17 15:06:14 +0600

    Add files via upload

---
+++ orOperation.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    int r=a|b;
+    cout<<r;
+
+cout<<"\nWant to do oparion more than two?\npress-1(yes)\npress-2(no)\n";
+int j;
+cin>>j;
+if(j==1)
+{
+     int aa,ba,ca;
+    cin>>aa>>ba>>ca;
+    int ra=aa|ba|ca;
+    cout<<ra;
+}
+
+}
